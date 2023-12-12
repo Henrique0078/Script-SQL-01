f196af8fa28911b523476a833403910c34a6cd83
Carlos Augusto <carlosaaugusto2002@outlook.com>
2023-12-12 14:48:47 -0300

    Update AutomaticController.ts

--- src/Controller/AutomaticController.ts
+++ src/Controller/AutomaticController.ts
@@ -27,58 +27,33 @@ export class migracao{
 			const empresasOld = await prismaBancaoAntigo.empresas.findMany({});
 			const empresasNew = await prismaBancao.empresas.findMany({});
 			// cria os prismas
-			for (const empresaNova of empresasNew) {
-				// Use 'of' for each element in 'empresasOld'
-				for (const empresaAntiga of empresasOld) {
-					await createDatabase(empresaNova.banco_empresa);
-					const { velhoPrisma, novoPrisma } = this.gerarPrismas(empresaAntiga.banco, empresaNova.banco_empresa);
-					const Service = new TrocaController();
-					await Service.troca(novoPrisma, velhoPrisma);
-				}
-			}
-			
-			// for (let i = 0; i < empresasNew.length; i++) {
-			// 	await createDatabase(empresasNew[i].banco_empresa);
-			// 	const {velhoPrisma,novoPrisma} = this.gerarPrismas(empresasOld[i],empresasNew[i]);
-			// 	const Service = new TrocaController();
-			// 	await Service.troca(novoPrisma,velhoPrisma);
 
-			// }
+			for (let i = 0; i < empresasNew.length; i++) {
+				await createDatabase(empresasNew[i].banco_empresa);
+				const {velhoPrisma,novoPrisma} = this.gerarPrismas(empresasOld[i],empresasNew[i]);
+				const Service = new TrocaController();
+				await Service.troca(novoPrisma,velhoPrisma);
+
+			}
 		} catch (error:any) {
 			throw new ErrorResponse(500,"FUDEU TUDO: " + error);
 		}
 	}
-	// gerarPrismas(empresaVelha:empresasVelhas,empresanova:empresasNovas){
-	// 	const velhoPrisma = new OldPrismaClient({
-	// 		datasources: {
-	// 			db: {
-	// 				url: `mysql://root:soft@1973@localhost:3306/${empresaVelha.banco}`
-	// 			}
-	// 		}
-	// 	});
-
-	// 	const novoPrisma = new NewPrismaClient({
-	// 		datasources: {
-	// 			db2: {
-	// 				url:`mysql://root:soft@1973@localhost:3306/${empresanova.banco_empresa}`
-	// 			}
-	// 		}
-	// 	});
-	// 	return {velhoPrisma,novoPrisma};
-	// }
-	gerarPrismas(empresaVelha: string, empresaNova: string){
+	gerarPrismas(empresaVelha:empresasVelhas,empresanova:empresasNovas){
+		const newUrl = `mysql://avelino:coxinha123@192.168.0.144:3306/${empresanova.banco_empresa}?schema=public`;
+		const oldUrl = `mysql://avelino:coxinha123@192.168.0.144:3306/${empresaVelha.banco}?schema=public`;
 		const velhoPrisma = new OldPrismaClient({
 			datasources: {
 				db: {
-					url: `mysql://root:soft@1973@localhost:3306/${empresaVelha}`
+					url: oldUrl,
 				}
 			}
 		});
-		
+
 		const novoPrisma = new NewPrismaClient({
 			datasources: {
 				db2: {
-					url:`mysql://root:soft@1973@localhost:3306/${empresaNova}`
+					url:newUrl,
 				}
 			}
 		});
